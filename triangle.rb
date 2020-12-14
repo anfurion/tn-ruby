ecef2b6aa229c3d1fb8599776d412e5ee3e5e6d1
Anton Sinyuk <anton.sinyuk@gmail.com>
2020-12-14 18:15:28 +0300

    New interface to triangle

--- triangle.rb
+++ triangle.rb
@@ -1,3 +1,10 @@
 def triangle_square(a, h)
-  1.0 / 2 * a * h
+  0.5 * a * h
 end
+
+puts 'Ввести основание треугольника'
+a = gets.to_f
+
+puts 'Ввести высоту треугольника'
+h = gets.to_f
+puts triangle_square(a, h)
